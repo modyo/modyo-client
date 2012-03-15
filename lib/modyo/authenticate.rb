e5da7fc31726d8af88bf0fdb80452e2e46bb84ef
Toño Silva Portell <jsilva@jcode.cl>
2012-03-14 21:56:53 -0300

    Fixed error on dependencies requirements

--- lib/modyo/authenticate.rb
+++ lib/modyo/authenticate.rb
@@ -1,6 +1,21 @@
 module Modyo
   module Authenticate
 
+    def self.included(base)
+
+      base.helper_method :link_to_modyo_profile
+      base.helper_method :modyo_profile_path
+      base.helper_method :modyo_site_key
+      base.helper_method :modyo_site_url
+      base.helper_method :modyo_account_url
+      base.helper_method :modyo_account_host
+      base.helper_method :modyo_site_main_url
+      base.helper_method :modyo_site_me_url
+      base.helper_method :modyo_admin_main_url
+
+    end
+
+
     module ClassMethods
       def authenticate_with_modyo
         if session[:user]
@@ -26,68 +41,45 @@ module Modyo
 
       # Helper methods
 
-          helper_method :link_to_modyo_profile
 
-          def link_to_modyo_profile(user, options = {})
+      def link_to_modyo_profile(user, options = {})
 
-            link_to (options[:text] || user.name), modyo_profile_path(user), :class => 'parent'
-          end
+        link_to (options[:text] || user.name), modyo_profile_path(user), :class => 'parent'
+      end
 
-          helper_method :modyo_profile_path
+      def modyo_profile_path(user)
+        "/#{modyo_site_key}/admin/people/members/show?membership_id=#{user.modyo_id}"
+      end
 
-          def modyo_profile_path(user)
-            "/#{modyo_site_key}/admin/people/members/show?membership_id=#{user.modyo_id}"
-          end
+      # Parameters
 
-          # Parameters
+      def modyo_site_key
+        MODYO['site_key']
+      end
 
-          helper_method :modyo_site_key
+      def modyo_site_url
+        MODYO['site_url']
+      end
 
-          def modyo_site_key
-            MODYO['site_key']
-          end
+      def modyo_account_url
+        MODYO['account_url']
+      end
 
+      def modyo_account_host
+        MODYO['account_host']
+      end
 
-          helper_method :modyo_site_url
+      def modyo_site_main_url
+        "#{modyo_site_url}/apps/#{MODYO['canvas_namespace']}"
+      end
 
-          def modyo_site_url
-            MODYO['site_url']
-          end
+      def modyo_site_me_url
+        "#{modyo_site_url}/me/#{MODYO['canvas_namespace']}"
+      end
 
-
-          helper_method :modyo_account_url
-
-          def modyo_account_url
-            MODYO['account_url']
-          end
-
-
-          helper_method :modyo_account_host
-
-          def modyo_account_host
-            MODYO['account_host']
-          end
-
-
-          helper_method :modyo_site_main_url
-
-          def modyo_site_main_url
-            "#{modyo_site_url}/apps/#{MODYO['canvas_namespace']}"
-          end
-
-
-          helper_method :modyo_site_me_url
-
-          def modyo_site_me_url
-            "#{modyo_site_url}/me/#{MODYO['canvas_namespace']}"
-          end
-
-
-          helper_method :modyo_admin_main_url
-
-          def modyo_admin_main_url
-            "#{modyo_account_url}/#{modyo_site_key}/admin/apps/#{MODYO['canvas_namespace']}"
-          end
+      def modyo_admin_main_url
+        "#{modyo_account_url}/#{modyo_site_key}/admin/apps/#{MODYO['canvas_namespace']}"
+      end
 
     end
 
