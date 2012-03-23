06dacc3fb4ab932be166ac4ee398aea224151063
Toño Silva Portell <jsilva@jcode.cl>
2012-03-23 16:01:39 -0300

    Exeption handling on Modyo Auth

--- lib/modyo/feed.rb
+++ lib/modyo/feed.rb
@@ -3,7 +3,7 @@ module Modyo
    def self.mail(user, options = {})
 
      @access_token = ::OAuth::AccessToken.new(self.class.consumer, user.oauth_token, user.oauth_secret)
-     response = @access_token.post("/api/mailer", {:recipient => user.modyo_id, :message => options[:message], :subjet => options[:subjet]})
+     response = @access_token.post("/api/mailer", {:recipient => user.modyo_id, :message => options[:message], :subject => options[:subject]})
      user_info = ::Nokogiri::XML(response.body)
 
      return user_info
--- lib/modyo/mailer.rb
+++ lib/modyo/mailer.rb
@@ -2,8 +2,8 @@ module Modyo
   class Mailer
     def self.mail(user, options = {})
 
-      @access_token = ::OAuth::AccessToken.new(self.class.consumer, user.oauth_token, user.oauth_secret)
-      response = @access_token.post("/api/mailer", {:recipient => user.modyo_id, :message => options[:message], :subjet => options[:subjet]})
+      @access_token = ::OAuth::AccessToken.new(Modyo::Session.consumer, user.oauth_token, user.oauth_secret)
+      response = @access_token.post("/api/mailer", {:recipient => user.modyo_id, :message => options[:message], :subject => options[:subject]})
       user_info = ::Nokogiri::XML(response.body)
       return user_info
     end
