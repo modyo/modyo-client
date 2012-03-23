184cb552d39347b803963d87c347471b819e523c
Toño Silva Portell <jsilva@jcode.cl>
2012-03-23 19:39:05 -0300

    Exeption handling on Modyo Auth

--- lib/modyo/mailer.rb
+++ lib/modyo/mailer.rb
@@ -2,7 +2,7 @@ module Modyo
   class Mailer
     def self.mail(user, options = {})
 
-      @access_token = ::OAuth::AccessToken.new(Modyo::Session.consumer, user.oauth_token, user.oauth_secret)
+      @access_token = ::OAuth::AccessToken.new(::Modyo::Session.consumer, user.oauth_token, user.oauth_secret)
       response = @access_token.post("/api/mailer", {:recipient => user.modyo_id, :message => options[:message], :subject => options[:subject]})
       user_info = ::Nokogiri::XML(response.body)
       return user_info
