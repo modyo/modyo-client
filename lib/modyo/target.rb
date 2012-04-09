f1bd9a71965041d4d4621637f9d256be14884ab4
Toño Silva Portell <jsilva@jcode.cl>
2012-04-09 13:08:56 -0300

    Exeption handling on Modyo Auth

--- lib/modyo/target.rb
+++ lib/modyo/target.rb
@@ -8,7 +8,7 @@ module Modyo
     def self.add_to_target(user, options = {})
 
       @access_token = ::OAuth::AccessToken.new(self.consumer, user.oauth_token, user.oauth_secret)
-      response = @access_token.post("/api/targets/add_to_target", {:target_id => 1103})
+      response = @access_token.post("/api/targets/add_to_target", {:target_id => options[:target_id]})
       target_response = ::Nokogiri::XML(response.body)
       return target_response
     end
