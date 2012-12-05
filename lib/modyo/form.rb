60ab94914a3a50ca5c6b9db88680cd197cc5f727
Javier <javier.poo@gmail.com>
2012-12-05 12:16:57 -0300

    add forms to sdk

---
+++ lib/modyo/form.rb
@@ -0,0 +1,17 @@
+module Modyo
+  class Form
+
+    def self.consumer
+      ::OAuth::Consumer.new(MODYO["key"], MODYO["secret"], :site => MODYO["site_url"])
+    end
+
+    def self.get_response(user, options = {})
+
+      @access_token = ::OAuth::AccessToken.new(self.consumer, user.oauth_token, user.oauth_secret)
+      response = @access_token.post("/api/form/form", { :form_id => options[:form_id] })
+      response_body = ::Nokogiri::XML(response.body)
+      return response_body
+    end
+
+  end
+end
